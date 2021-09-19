049dd484b1545f1f866aa7b4ae77694b2875bdfb
jinwoo <jintakk6531@gmail.com>
2021-09-19 12:15:21 +0900

    9/19

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
+<div class="menu">
+  <a v-for="(item,i) in menus" :key="i">{{item}}</a>
+</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  ㅎㅇ
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div v-for="(item,i) in products" :key="i">
+    {{item}}
+    <button @click="increase">허위매물신고</button> <span>신고수 : {{num}}</span>
+  </div>
+
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      num: 0,
+      menus: ['Home', 'Products', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods: {
+    increase(){
+      this.num += 1;
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -23,5 +44,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
