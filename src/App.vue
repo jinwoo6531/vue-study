a61c2f31665696435681e7ca5919827974b2b2a6
jinwoo <jintakk6531@gmail.com>
2021-09-22 11:14:04 +0900

    9/22 코딩애플 vue 기초 study 완료

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
+
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="modal = false">닫기</button>
+    </div>
+  </div>
+
+
 <div class="menu">
   <a v-for="(item,i) in menus" :key="i">{{item}}</a>
 </div>
-  <img alt="Vue logo" src="./assets/logo.png">
+  
 
-  <div v-for="(item,i) in products" :key="i">
-    {{item}}
-    <button @click="increase">허위매물신고</button> <span>신고수 : {{num}}</span>
+  
+  <div  v-for="(item,i) in oneroom" :key="i">
+    <img :src="item.image" class="room-img">
+    <h4 @click="modal = true">{{item.title}}</h4>
+    <p>{{item.price}} 만원</p>
+    
+    <br/><br/>
   </div>
 
 </template>
 
 <script>
 
+import data from './post';
+
 export default {
   name: 'App',
   data() {
     return {
-      num: 0,
+      oneroom: data,
+      modal: false,
+      num: [0,0,0],
       menus: ['Home', 'Products', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸']
     }
@@ -34,6 +52,31 @@ export default {
 </script>
 
 <style>
+
+body{
+  margin:0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width:100%;
+  background: white;
+  border-radius: 8px;
+  padding:20px;
+}
+
+
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
